d0fae5916eed4c47ed68e4c77bc4dbea91cb79df
Karel Kranich <karelkranich@gmail.com>
2021-09-15 17:08:44 +0300

    font & laius

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,8 @@ export default function Projects() {
 
     const fetchData = async () => {
       const results = await axios(
-        "https://henrikutsar.ee/admin/wp-json/acf/v3/projektid/"
+        // "https://henrikutsar.ee/admin/wp-json/acf/v3/projektid/"
+        "https://henrikutsar.ee/admin/wp-json/wp/v2/projektid/"
       );
       setPosts(results.data);
     };
@@ -26,29 +27,40 @@ export default function Projects() {
   }, []);
 
   return (
-    <section className="image-section">
-      <div className="grid-container">
-        {posts &&
-          posts.map((post) => (
-            <div
-              // style={{ width: `${post.acf.laius}` + "%" }}
-              key={post.id}
-              className="grid-item "
-            >
-              <Link to={`/${post.acf.slug}`}>
-                <Hover>{post.acf.projekti_pealkiri}</Hover>
-                {/* {posts.acf.thumbnaili_foto && */}
-                {/* posts.map((post) => ( */}
-                <img src={post.acf.thumbnaili_foto.url} alt="Savant" />
-                {/* ))} */}
-              </Link>
-              <div className="project-title project-title-2">
-                {/* {post.title.rendered} */}
-                {console.log(post.acf.slug)}
+    <>
+      <section className="image-section ">
+        <div className="grid-container">
+          {posts &&
+            posts.map((post) => (
+              <div
+                style={{ width: `${post.acf.pildi_laius}` + "%" }}
+                key={post.id}
+                className="grid-item "
+              >
+                {console.log(post.acf.pildi_laius)}
+                <Link to={`/${post.slug}`}>
+                  <Hover>{post.acf.projekti_pealkiri}</Hover>
+
+                  <img
+                    className="bigger-screen-view"
+                    src={post.acf.thumbnaili_foto.url}
+                    alt={post.acf.projekti_pealkiri}
+                  />
+                  <img
+                    className="smaller-screen-view"
+                    src={post.acf.thumbnaili_foto.sizes.medium_large}
+                    alt={post.acf.projekti_pealkiri}
+                  ></img>
+                  {/* ))} */}
+                </Link>
+
+                <div className="project-title project-title-2">
+                  {/* {post.title.rendered} */}
+                </div>
               </div>
-            </div>
-          ))}
-      </div>
-    </section>
+            ))}
+        </div>
+      </section>
+    </>
   );
 }
