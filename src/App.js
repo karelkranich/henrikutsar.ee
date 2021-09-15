763ee45debedfdcc935f2a41872d5ad0bc2583eb
Karel Kranich <karelkranich@gmail.com>
2021-09-15 12:50:50 +0300

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter basename="/">
       <Header />
       <Grid />
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence >
         <Switch>
           <Route path="/" component={HomePage} exact></Route>
           <Route path={`/:slug`} component={ProjectView}></Route>
