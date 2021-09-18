742c6aee14cc6e8bea9ce328105adefad29b1d6e
Karel Kranich <karelkranich@gmail.com>
2021-09-18 16:31:09 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import HomePage from "./components/HomePage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ProjectView from "./components/ProjectView";
-
+import Header from "./components/Header";
+import Grid from "./components/Grid";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <BrowserRouter basename="/">
+      <Header />
+      <Grid />
       <AnimatePresence>
         <Switch>
           <Route path="/" component={HomePage} exact></Route>
