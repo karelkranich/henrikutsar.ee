5de51a5edfd5386fa44418151a10f813d93068d3
Karel Kranich <karelkranich@gmail.com>
2022-02-04 13:07:01 +0200

    Added Material UI, react router v6 changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Grid from "./Grid";
+import { Outlet } from "react-router";
 
 export default function HomePage() {
   return (
     <div>
       <Header />
       <Grid />
+      <Outlet />
     </div>
   );
 }
